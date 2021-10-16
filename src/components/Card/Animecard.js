30fc03e1cc47e880998105e8994b66528378f8f4
Sebin Chacko Mathew <sebinsebzz2002@gmail.com>
2021-10-16 14:57:19 +0530

    finished project

--- src/components/Card/Animecard.js
+++ src/components/Card/Animecard.js
@@ -4,9 +4,9 @@ import React from "react";
 
 function Animecard({ value }) {
   return (
-    <div className="my-2">
+    <div className="my-2 px-5">
       <h3 className=" mt-2 font-bold text-green-500 text-sm">{value.type}</h3>
-      <div className="w-24 mr-10">
+      <div className="w-24 ">
         <img className="" src={value.image_url} alt="Thumbnail" />
         <h5 className="text-center mt-2 font-bold text-green-500 text-sm">
           {value.title}
--- src/components/Webapp/Webapp.js
+++ src/components/Webapp/Webapp.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Animecard from "../Card/Animecard";
 import jikan from "../../API/jikan";
+import ReactLoading from "react-loading";
 
 const Webapp = () => {
   // useState items
   const [searchQuery, setSearchQuery] = useState([]);
   const [animeList, setAnimeList] = useState([]);
   const [mangaList, setMangaList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // calls functions to fetch deatils from API based on searchQuery
   const handleSearch = (e) => {
@@ -27,10 +29,12 @@ const Webapp = () => {
         },
       })
       .then((response) => {
-        console.log(response.data.results);
+        // console.log(response.data.results);
         setAnimeList(response.data.results);
+        setLoading(false);
       });
   };
+
   const fetchManga = async (searchQuery) => {
     await jikan
       .get("search/manga", {
@@ -41,38 +45,50 @@ const Webapp = () => {
         },
       })
       .then((response) => {
-        console.log(response.data.results);
+        // console.log(response.data.results);
         setMangaList(response.data.results);
       });
   };
 
+  useEffect(() => {
+    fetchAnime("anime");
+    fetchManga("manga");
+  }, []);
   return (
     <>
       <div className="">
-        <div className="webapp ">
+        <div className="webapp  ">
           <Navbar
             className=""
             handleSearch={handleSearch}
             searchQuery={searchQuery}
             setSearchQuery={setSearchQuery}
           />
-          <h2 className="font-bold text-green-500 text-lg mx-10 pt-2">
-            Search Results....
-          </h2>
 
           {/* Animecard */}
-          <div className="flex">
-            <div className="flex mx-10 scrollbar">
-              {animeList.map((anime) => (
-                <Animecard value={anime} key={anime.mal_id} />
-              ))}
+          {loading ? (
+            <div className="flex justify-center p-48 ">
+              <ReactLoading
+                type={"bars"}
+                color={"rgba(16, 185, 129)"}
+                height={100}
+                width={100}
+              />
             </div>
-            <div className="flex mx-10 scrollbar">
-              {mangaList.map((manga) => (
-                <Animecard value={manga} key={manga.mal_id} />
-              ))}
-            </div>
-          </div>
+          ) : (
+            <>
+              <div className="flex mx-10 scrollbar h-60 mt-10">
+                {animeList.map((anime) => (
+                  <Animecard value={anime} key={anime.mal_id} />
+                ))}
+              </div>
+              <div className="flex mx-10 scrollbar  h-60">
+                {mangaList.map((manga) => (
+                  <Animecard value={manga} key={manga.mal_id} />
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
